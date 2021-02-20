db3cde6f16052944db623821be3eb79d1c039ca5
User <user@MacBook-Pro-Apple.local>
2021-02-20 11:57:17 +0500

    fix

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// let slides = document.querySelectorAll(".slide")
-// let buttons = document.querySelectorAll('.slide-btn')
-//
-// for (let i = 0; i < buttons.length; i++) {
-//     buttons[i].addEventListener("click", () => {
-//         for (let x = 0; x < slides.length; x++) {
-//             slides[x].classList.remove("active")
-//         }
-//         slides[i].classList.add("active")
-//     })
-// }
-
 let button = document.querySelector(".modal-btn")
 let modalWindow = document.querySelector(".modal")
 let overlay = document.querySelector(".modal__overlay")
@@ -29,66 +17,72 @@ closeButton.addEventListener("click", () => {
 let buttonLeft = document.querySelector(".slide-arrow_left")
 let buttonRight = document.querySelector(".slide-arrow_right")
 let currentSlideId = 1
-let buttonsId = 1
-let buttons = document.querySelector(`.slide-btn[data-id="${buttonsId}"]`)
-buttonLeft.addEventListener("click", () => {
+slideToLeft(1)
 
-
-    let nextSlideId = currentSlideId + 1
-    if (nextSlideId > document.querySelectorAll(`.slide2`).length) {
-        nextSlideId = 1
-    }
-    console.log(nextSlideId)
-
+function slideToLeft(nextSlideId) {
+    clear()
     let currentSlide = document.querySelector(`.slide2[data-id="${currentSlideId}"]`)
     currentSlide.classList.add("to-left")
     currentSlide.classList.remove("from-right")
-
-
     let nextSlide = document.querySelector(`.slide2[data-id="${nextSlideId}"]`)
     nextSlide.classList.add("from-right")
-
     currentSlideId = nextSlideId
-
-    currentSlide.classList.remove("from-left","to-right")
-    nextSlide.classList.remove("from-left","to-right")
-
-
-    if (currentSlide.length === buttons.length) {
-    for (let i = currentSlide.length - 1; i >= 0; i--) {
-    currentSlide[i].id = buttons[i].id;
-  }
 }
 
-})
+function slideToRight(nextSlideId) {
+    clear()
+    let currentSlide = document.querySelector(`.slide2[data-id="${currentSlideId}"]`)
+    currentSlide.classList.add("from-left")
+    currentSlide.classList.remove("from-left")
+    currentSlide.classList.add("to-right")
+    let nextSlide = document.querySelector(`.slide2[data-id="${nextSlideId}"]`)
+    nextSlide.classList.add("from-left")
+    currentSlideId = nextSlideId
+}
 
-buttonRight.addEventListener("click", () => {
+function clear() {
+    let slides = document.querySelectorAll(`.slide2`)
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("from-left", 'from-right', 'to-left', 'to-right')
+    }
+}
 
+
+buttonLeft.addEventListener("click", () => {
     let nextSlideId = currentSlideId + 1
     if (nextSlideId > document.querySelectorAll(`.slide2`).length) {
         nextSlideId = 1
     }
-    console.log(nextSlideId)
-
-    let currentSlide = document.querySelector(`.slide2[data-id="${currentSlideId}"]`)
-
-    currentSlide.classList.add("from-left")
-    currentSlide.classList.remove("from-left")
-    currentSlide.classList.add("to-right")
-
-    let nextSlide = document.querySelector(`.slide2[data-id="${nextSlideId}"]`)
-    nextSlide.classList.add("from-left")
-
-    currentSlideId = nextSlideId
-
-
-
-
+    slideToLeft(nextSlideId)
 })
-// когда кликаешь на стрелку id текущего слайда должно совпасть с id нужной кнопки
-//
-// currentSlide data-id
-// slide-btn data-id
+
+buttonRight.addEventListener("click", () => {
+    let nextSlideId = currentSlideId - 1
+    if (nextSlideId === 0) {
+        nextSlideId = document.querySelectorAll(`.slide2`).length - 1
+    }
+    slideToRight(nextSlideId)
+})
+
+let btns = document.querySelectorAll(".slide-btn")
+
+for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", () => {
+        let nextSlideId = btns[i].getAttribute("data-id")
+        for (let x = 0; x < btns.length; x++) {
+            btns[x].classList.remove("active")
+        }
+        btns[i].classList.add("active")
+
+        if (currentSlideId > nextSlideId) {
+            slideToRight(nextSlideId)
+        } else if (nextSlideId > currentSlideId) {
+            slideToLeft(nextSlideId)
+        }
+    })
+}
+
+
 
 
 
