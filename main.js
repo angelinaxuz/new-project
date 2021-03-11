765bb13ec80f1503dc99f657a1d2850c090f9a38
User <user@MacBook-Pro-Apple.local>
2021-03-11 17:53:16 +0500

    fix

--- main.js
+++ main.js
@@ -16,95 +16,56 @@ closeButton.addEventListener("click", () => {
 
 let buttonLeft = document.querySelector(".slide-arrow_left")
 let buttonRight = document.querySelector(".slide-arrow_right")
-let currentSlideId = 1
-slideToLeft(1)
 
-function slideToLeft(nextSlideId) {
-    clear()
-    let currentSlide = document.querySelector(`.slide2[data-id="${currentSlideId}"]`)
-    currentSlide.classList.add("to-left")
-    currentSlide.classList.remove("from-right")
-    let nextSlide = document.querySelector(`.slide2[data-id="${nextSlideId}"]`)
-    nextSlide.classList.add("from-right")
-    currentSlideId = nextSlideId
+
+function getMoveValue(index,w){
+    return (0-index)*w
 }
 
-function slideToRight(nextSlideId) {
-    clear()
-    let currentSlide = document.querySelector(`.slide2[data-id="${currentSlideId}"]`)
-    currentSlide.classList.add("from-left")
-    currentSlide.classList.remove("from-left")
-    currentSlide.classList.add("to-right")
-    let nextSlide = document.querySelector(`.slide2[data-id="${nextSlideId}"]`)
-    nextSlide.classList.add("from-left")
-    currentSlideId = nextSlideId
+function getNextSlideIndex(slideIndex) {
+    return slideIndex + 1
 }
 
-function clear() {
-    let slides = document.querySelectorAll(`.slide2`)
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].classList.remove("from-left", 'from-right', 'to-left', 'to-right')
-    }
+function getSlideWidth (slideIndex) {
+    let slide = document.querySelector(`.slide:nth-child(${slideIndex+1})`)
+    return slide.clientWidth
 }
 
+function moveWrapper(x) {
+    let slideWrapper = document.querySelector(".slider__inner")
+    slideWrapper.style.transform = `translateX(${x}px)`
+}
 
+let currentSlideIndex = 1
 buttonLeft.addEventListener("click", () => {
-    let nextSlideId = currentSlideId + 1
-    if (nextSlideId > document.querySelectorAll(`.slide2`).length) {
-        nextSlideId = 1
-    }
-    slideToLeft(nextSlideId)
+    let w = getSlideWidth(currentSlideIndex)
+    let x = getMoveValue(currentSlideIndex, w)
+    moveWrapper(x)
+    currentSlideIndex = getNextSlideIndex(currentSlideIndex)
 })
 
-buttonRight.addEventListener("click", () => {
-    let nextSlideId = currentSlideId - 1
-    if (nextSlideId === 0) {
-        nextSlideId = document.querySelectorAll(`.slide2`).length - 1
-    }
-    slideToRight(nextSlideId)
-})
 
-let btns = document.querySelectorAll(".slide-btn")
+function getMoveReverseValue(index,w){
+    return (0+index)*w
+}
+getMoveReverseValue(2)
+debugger
 
-
-for (let i = 0; i < btns.length; i++) {
-
-    btns[i].addEventListener("click", () => {
-
-        let nextSlideId = btns[i].getAttribute("data-id")
-        for (let x = 0; x < btns.length; x++) {
-            btns[x].classList.remove("active")
-        }
-        btns[i].classList.add("active")
-
-        if (currentSlideId > nextSlideId) {
-            slideToRight(nextSlideId)
-        } else if (nextSlideId > currentSlideId) {
-            slideToLeft(nextSlideId)
-        }
-    })
+function getPrevSlideIndex(slideIndex){
+    return slideIndex - 1
+}
+getPrevSlideIndex(2)
+debugger
+function moveWrapperRight(y) {
+    let slideWrapper = document.querySelector(".slider__inner")
+    slideWrapper.style.transform = `translateX(${x}px)`
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+buttonRight.addEventListener("click", ()=> {
+    let w = getSlideWidth(currentSlideIndex)
+    let y = getMoveReverseValue(currentSlideIndex, w)
+    moveWrapperRight(y)
+    currentSlideIndex = getPrevSlideIndex(currentSlideIndex)
+})
 
 
